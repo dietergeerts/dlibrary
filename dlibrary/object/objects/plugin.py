d5f727e24c23ed2ab1a4f1cb785b39a79b0ac73a
Dieter Geerts <dieter@dworks.be>
2015-01-27 21:09:35 +0100

    #4: Added drawing file loading & saving.

--- dlibrary/object/objects/plugin.py
+++ dlibrary/object/objects/plugin.py
@@ -1,3 +1,4 @@
+import os
 from dlibrary.utility import xmltodict
 from dlibrary.utility.exception import VSException
 from dlibrary.utility.singleton import SingletonMeta
@@ -6,38 +7,60 @@ import vs
 
 class PlugIn(object, metaclass=SingletonMeta):
     def __init__(self):
-        self.__name = None; """@type: str"""
+        self.__name = None
 
-    def __load_info(self):
+    def __get_name(self) -> str:
         if self.__name is None:
             succeeded, name, record = vs.GetPluginInfo()
             if not succeeded: raise VSException('GetPluginInfo')
-            else: self.__name = name;
+            else: self.__name = name
+        return self.__name
 
-    def __get_file_name(self, name: str) -> str: return self.__name + name + '.xml'
+    def __get_plugin_file_name(self, name: str) -> str:
+        try: file_name = self.__get_name() + name + '.xml'
+        except VSException: raise
+        else: return file_name
 
-    def __get_file_path(self, name: str) -> str:
+    def __get_drawing_file_name(self) -> str:
+        try: file_name = self.__get_name() + '.xml'
+        except VSException: raise
+        else: return file_name
+
+    def __get_plugin_file_path(self, name: str) -> str:
         exists, path = vs.FindFileInPluginFolder(name)
         if not exists: raise FileNotFoundError
         else: return path + name
 
+    def __get_drawing_file_path(self, name: str) -> str: return vs.GetFPathName() + name
+
     def load_plugin_file(self, name: str, list_elements: set=None) -> dict:
-        try: self.__load_info()
+        try:
+            path = self.__get_plugin_file_path(self.__get_plugin_file_name(name))
+            content = xmltodict.load(path, list_elements or set())
         except VSException: raise
-        else:
-            try: path = self.__get_file_path(self.__get_file_name(name))
-            except FileNotFoundError: raise
-            else:
-                try: content = xmltodict.load(path, list_elements or set())
-                except FileNotFoundError: raise
-                except PermissionError: raise
-                except OSError: raise
-                else: return content
+        except FileNotFoundError: raise
+        except PermissionError: raise
+        except OSError: raise
+        else: return content
 
     def save_plugin_file(self, content: dict, name: str):
-        try: self.__load_info()
+        try: path = self.__get_plugin_file_path(self.__get_plugin_file_name(name))
         except VSException: raise
-        else:
-            try: path = self.__get_file_path(self.__get_file_name(name))
-            except FileNotFoundError: raise
-            else: xmltodict.save(content, path)
+        except FileNotFoundError: raise
+        else: xmltodict.save(content, path)
+
+    def load_drawing_file(self, list_elements: set=None) -> dict:
+        try:
+            path = self.__get_drawing_file_path(self.__get_drawing_file_name())
+            if not os.path.isfile(path): content = {'prefs': {}}
+            else: content = xmltodict.load(path, list_elements or set())
+        except VSException: raise
+        except FileNotFoundError: raise
+        except PermissionError: raise
+        except OSError: raise
+        else: return content
+
+    def save_drawing_file(self, content: dict):
+        try: path = self.__get_drawing_file_path(self.__get_drawing_file_name())
+        except VSException: raise
+        else: xmltodict.save(content, path)
