3a8a01685b96c4e134d6da2dc57e4356e11689c5
Dieter Geerts <dieter@dworks.be>
2015-03-28 16:28:24 +0100

    #7: Added the ability to disable an edit text control.

--- dlibrary/dialog/controls/edittext.py
+++ dlibrary/dialog/controls/edittext.py
@@ -10,9 +10,10 @@ class EditText(AbstractFieldControl):
     def get_align_mode(cls, layout: int) -> int: return AlignModeEnum.RESIZE
 
     def __init__(self, dialog_id: int, control_id: int, help_text: str, data_parent: AbstractDataContext,
-                 data_context: str, data_value: str, data_items: str, width: int):
+                 data_context: str, data_value: str, data_items: str, width: int, disabled: bool):
         super().__init__(dialog_id, control_id, help_text, data_parent, data_context, data_value, data_items)
         vs.CreateEditText(dialog_id, control_id, self._value, width)
+        vs.EnableTextEdit(dialog_id, control_id, not disabled)
 
     def _set_control_value(self, value): vs.SetItemText(self._dialog_id, self.control_id, value)
 
@@ -29,10 +30,11 @@ class EditText(AbstractFieldControl):
 #       optional: @data-context -> str (property name of parent data-context) -> ObservableField
 #       required: @data-value -> str (property name of data-context or of an item) -> ObservableField
 #       optional: @data-items -> str (property name of data-context) -> ObservableList
-#       optional: @width -> int (in chars) || 20/>
+#       optional: @width -> int (in chars) || 20
+#       optional: @disabled -> bool || False/>
 # ----------------------------------------------------------------------------------------------------------------------
 
 def create(dialog_id: int, control_id: int, data: dict, data_parent: AbstractDataContext) -> EditText:
     return EditText(
         dialog_id, control_id, data.get('@help', ''), data_parent, data.get('@data-context', ''), data['@data-value'],
-        data.get('@data-items', ''), data.get('@width', 20))
+        data.get('@data-items', ''), data.get('@width', 20), data.get('@disabled', False))
