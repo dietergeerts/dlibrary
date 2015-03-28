50067a64f33dd6c6a6400b45f75b21f494421d04
Dieter Geerts <dieter@dworks.be>
2015-03-28 21:26:13 +0100

    #11: Change get_default_model to create_new_mode, as that is what it want that method to do.

--- dlibrary/dialog/viewmodel.py
+++ dlibrary/dialog/viewmodel.py
@@ -10,11 +10,11 @@ class AbstractViewModel(object):
 
 
 class ViewModelList(object):
-    def __init__(self, model_list: list, abstract_view_model: callable, get_default_model: callable):
+    def __init__(self, model_list: list, abstract_view_model: callable, create_new_model: callable):
         self.__items = LinkedObservableList(model_list, abstract_view_model, lambda view_model: view_model.model)
         self.__selected_items = ObservableList()
         self.__abstract_view_model = abstract_view_model
-        self.__get_default_model = get_default_model
+        self.__create_new_model = create_new_model
         self.__new_item = ObservableField()
         self.__setup_new_item()
 
@@ -29,4 +29,4 @@ class ViewModelList(object):
 
     def add_item(self): self.items.append(self.new_item.value); self.__setup_new_item()
 
-    def __setup_new_item(self): self.new_item.value = self.__abstract_view_model(self.__get_default_model())
+    def __setup_new_item(self): self.new_item.value = self.__abstract_view_model(self.__create_new_model())
