06a0cb6c505280bdc82e182574d4d3f5eba46ef0
Dieter Geerts <dieter@dworks.be>
2015-07-03 19:59:00 +0200

    #28: The edit text control can now be multiline, so an edit text box.

--- dlibrary/dialog/controls/edittext.py
+++ dlibrary/dialog/controls/edittext.py
@@ -12,10 +12,13 @@ class EditText(AbstractFieldControl):
         return AlignMode.RESIZE
 
     def __init__(self, dialog_id: int, control_id: int, help_text: str, data_parent: AbstractDataContext,
-                 data_context: str, data_disabled: str, data_value: str, data_items: str, width: int):
+                 data_context: str, data_disabled: str, data_value: str, data_items: str, width: int, height: int):
         super().__init__(dialog_id, control_id, help_text, data_parent, data_context, data_disabled, data_value,
                          data_items)
-        vs.CreateEditText(dialog_id, control_id, self._value, width)
+        if height > 1:
+            vs.CreateEditTextBox(dialog_id, control_id, self._value, width, height)
+        else:
+            vs.CreateEditText(dialog_id, control_id, self._value, width)
 
     def _set_control_value(self, value):
         vs.SetItemText(self._dialog_id, self.control_id, value)
@@ -29,15 +32,17 @@ class EditText(AbstractFieldControl):
 
 # ----------------------------------------------------------------------------------------------------------------------
 #   <edit-text
-#       optional: @help -> str
-#       optional: @data-context -> str (property name of parent data-context) -> ObservableField
+#       optional: @help          -> str
+#       optional: @data-context  -> str (property name of parent data-context) -> ObservableField
 #       optional: @data-disabled -> str (property name of data-context) -> ObservableMethod() -> bool
-#       required: @data-value -> str (property name of data-context or of an item) -> ObservableField
-#       optional: @data-items -> str (property name of data-context) -> ObservableList
-#       optional: @width -> int (in chars) || 20/>
+#       required: @data-value    -> str (property name of data-context or of an item) -> ObservableField
+#       optional: @data-items    -> str (property name of data-context) -> ObservableList
+#       optional: @width         -> int (in chars) || 20
+#       optional: @height        -> int (in rows) || 1/>
 # ----------------------------------------------------------------------------------------------------------------------
 
 def create(dialog_id: int, control_id: int, data: dict, data_parent: AbstractDataContext) -> EditText:
     return EditText(
         dialog_id, control_id, data.get('@help', ''), data_parent, data.get('@data-context', ''),
-        data.get('@data-disabled', ''), data['@data-value'], data.get('@data-items', ''), data.get('@width', 20))
+        data.get('@data-disabled', ''), data['@data-value'], data.get('@data-items', ''), data.get('@width', 20),
+        data.get('@height', 1))
