f1ba70c8e5fc6540d2415101c977f64b44452c38
Dieter Geerts <dieter@dworks.be>
2015-07-03 18:22:59 +0200

    #27: Filename can be retrieved from the active document.

--- dlibrary/document.py
+++ dlibrary/document.py
@@ -3,6 +3,11 @@ import vs
 
 
 class Document(object, metaclass=SingletonMeta):
+
     @property
     def saved(self) -> bool:
         return vs.GetFName() != vs.GetFPathName()
+
+    @property
+    def filename(self) -> str:
+        return vs.GetFName()
