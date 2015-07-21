27e4bd00df5d7aa16ddfbe0c34c43db9a68ba061
Dieter Geerts <dieter@dworks.be>
2015-07-21 12:18:45 +0200

    #41: Added install script to include with plugins.

---
+++ MANUAL_INSTALL.md
@@ -0,0 +1,18 @@
+VW DLibrary
+========================================================================================================================
+
+Common library for VW plug-ins.
+
+## MANUAL INSTALL ######################################################################################################
+
+Only install if you have no or an older version of this library and the plugin needs this version.
+
+**TIP**: The version number can always be found in the `__init__.py` file in the `dlibrary` folder.
+
+To manually install this library in order for Vectorworks to use, follow these steps:
+ 
+- Copy the `dlibrary` folder to a folder of your chose.
+- Add that folder, where the `dlibrary` folder resides, to the Python Script Settings in Vectorworks.
+- Restart Vectorworks.
+
+If done right, Vectorworks will now pick up the library for your plugins to use.
---
+++ install.py
@@ -0,0 +1,172 @@
+import os
+import re
+import shutil
+import urllib.request
+from xml.etree import ElementTree
+import vs
+
+# This installation file can be used for your own plugin. It contains several possible steps you can include. This will
+# make it easier for your users to install. To add certain steps/functionality, just (un)comment them at the end.
+# An install script must go at the top level of your .zip file in order for Vectorworks to pick it up. For more
+# information about install files, see: http://developer.vectorworks.net/index.php/VS:Implementing_Installation_Script
+
+
+# SET DESTINATION FOLDER ###############################################################################################
+# By default, VW will copy all plugin files into the users' plugin folder. This is not always desired. For example when
+# more than one user will use your plugin, the office can place them on a network drive and point each VW installation
+# to that drive in order for VW to pick up the plugin and be able to use it. Including this step will enable the user to
+# select a custom folder so that the files will be copied there, or tell that he already did this, to delete the files
+# from the users' plugin folder.
+# ----------------------------------------------------------------------------------------------------------------------
+
+def set_destination_folder_for(plugin_folder_name: str):
+
+    def ask_where_to_install() -> int:
+        return vs.AlertQuestion(
+            'Do you want to install this plugin in your user folder?',
+            'If more than one user will use this plugin, you can install it on your network drive once for all.',
+            1, 'Yes', 'No, I already placed it on my network', 'No, let me choose a custom folder', '')
+
+    def install_in_user_folder():
+        pass
+
+    def install_in_custom_folder():
+        user_plugin_folder = vs.GetFolderPath(-2)
+        # result, directory_path = vs.GetFolder('Please select the install directory. '
+        #                                       'Cancelling will install in your user folder.')
+        result, directory_path = 0, 'D:\\DWorks\\Vectorworks\\2015\\Resources\\Plug-Ins'
+        if result == 0:  # Directory is chosen with ok button.
+            if os.path.exists(os.path.join(directory_path, plugin_folder_name)):
+                shutil.rmtree(os.path.join(directory_path, plugin_folder_name))
+            shutil.move(os.path.join(user_plugin_folder, plugin_folder_name), directory_path)
+            vs.AlrtDialog('Don\'t forget to Let Vectorworks know about your custom folder!')
+
+    def already_installed_in_custom_folder():
+        user_plugin_folder = vs.GetFolderPath(-2)
+        shutil.rmtree(os.path.join(user_plugin_folder, plugin_folder_name))
+
+    {
+        1: install_in_user_folder,
+        2: install_in_custom_folder,
+        0: already_installed_in_custom_folder
+    }.get(ask_where_to_install())()
+
+
+# DLIBRARY INSTALL #####################################################################################################
+# When your plugin uses this library, it also needs to be installed and added to the Python search paths. Off course we
+# need to check the version that could be already installed, and only install if a newer version is required.
+# ----------------------------------------------------------------------------------------------------------------------
+
+def update_to_or_install_dlibrary_version(required_version: str):
+
+    def update_dlibrary_needed(libraries_folder: str) -> bool:
+        with open(os.path.join(libraries_folder, 'dlibrary', '__init__.py')) as package_file:
+            pkg_info = package_file.read()
+
+        version_info = [int(info) for info in re.search(r'__version__ = \'.+?\'', pkg_info).group(0)[15:-1].split('.')]
+        version_info_needed = [int(info) for info in required_version.split('.')]
+
+        # Version info like [2015, 0, 0]
+        if version_info[0] > version_info_needed[0]:
+            return False
+        elif version_info[0] < version_info_needed[0]:
+            return True
+        elif version_info[1] > version_info_needed[1]:
+            return False
+        elif version_info[1] < version_info_needed[1]:
+            return True
+        elif version_info[2] > version_info_needed[2]:
+            return False
+        elif version_info[2] < version_info_needed[2]:
+            return True
+        else:
+            return False
+
+    def update_dlibrary(libraries_folder: str):
+        shutil.rmtree(os.path.join(libraries_folder, 'dlibrary'))
+        install_dlibrary(libraries_folder)
+
+    def install_dlibrary(libraries_folder: str):
+        dlibrary_url = 'https://bitbucket.org/dieterdworks/vw-dlibrary/get/v' + required_version + '.zip'
+        dlibrary_zip = os.path.join(libraries_folder, 'dlibrary_repo.zip')
+        try:
+            # vs.ProgressDlgOpen('DLibrary installation', False)
+            # vs.ProgressDlgSetMeter('Installing dlibrary v' + required_version + '...')
+            vs.AlrtDialog('We\'ll start installing `dlibrary`. This can take a while.')
+            urllib.request.urlretrieve(dlibrary_url, dlibrary_zip)
+            shutil.unpack_archive(dlibrary_zip, libraries_folder)
+            unzipped_dir = get_unzipped_dir(libraries_folder)
+            shutil.move(os.path.join(libraries_folder, unzipped_dir, 'dlibrary'), libraries_folder)
+        except:
+            vs.AlrtDialog('The library \'dlibrary\' couldn\'t be downloaded. Please install it manually.')
+            raise
+        finally:
+            unzipped_dir = get_unzipped_dir(libraries_folder)
+            if unzipped_dir != '':
+                shutil.rmtree(os.path.join(libraries_folder, unzipped_dir))
+            if os.path.exists(dlibrary_zip):
+                os.remove(dlibrary_zip)
+            # vs.ProgressDlgClose()
+
+    def get_unzipped_dir(libraries_folder: str):
+        unzipped_repo_folders = [f for f in os.listdir(libraries_folder) if f.startswith('dieterdworks-vw-dlibrary')]
+        return unzipped_repo_folders[0] if len(unzipped_repo_folders) > 0 else ''
+
+    vs.AlrtDialog(
+        'This plugin requires the \'dlibrary\' library, version ' + required_version + '. We would like to install or '
+        'update it. Please select its directory or the one where we can install it in the next dialog. Cancel that '
+        'dialog to manually install the library.')
+
+    # result, directory_path = vs.GetFolder(
+    #     'Please select the directory where we can find dlibaray, or where we may install it. '
+    #     'Also make sure we have write access to that folder, otherwise the installation will fail!')
+    result, directory_path = 0, 'D:\\DWorks\\Vectorworks\\2015\\Plug-In libraries'
+
+    if result == 0:  # Directory is set with ok button.
+        if os.path.exists(os.path.join(directory_path, 'dlibrary')):
+            if update_dlibrary_needed(directory_path):
+                update_dlibrary(directory_path)
+        else:
+            install_dlibrary(directory_path)
+    else:
+        vs.AlrtDialog('Don\'t forget to manually install `dlibrary`. It can be found at: '
+                      'https://bitbucket.org/dieterdworks/vw-dlibrary/get/v' + required_version + '.zip')
+
+
+# ADD TO WORKSPACES ####################################################################################################
+# In most cases, we also want to add our plugins to the users' workspace, so he doens't have to go through that. This
+# will get him started immediately.
+# ----------------------------------------------------------------------------------------------------------------------
+# REMARK: This doesn't work. We'll just show a message to the user he has to do it untill we find out how to do it!
+# ----------------------------------------------------------------------------------------------------------------------
+
+def add_plugins_to_workspaces(plugins_structure: dict):
+
+    def get_or_create_and_get_user_workspace_path():
+        user_workspace_path = vs.GetFolderPath(-4)
+        if not os.path.exists(os.path.join(user_workspace_path)):
+            os.makedirs(os.path.join(user_workspace_path))
+        return user_workspace_path
+
+    def get_all_user_workspaces():
+        user_workspace_path = get_or_create_and_get_user_workspace_path()
+        return [os.path.join(user_workspace_path, ws) for ws in os.listdir(user_workspace_path) if ws.endswith('.vww')]
+
+    def add_plugins_to_workspace(workspace_path: str):
+        workspace_tree = ElementTree.parse(workspace_path)
+        for _, _ in plugins_structure.items():
+            pass
+        workspace_tree.write(workspace_path)
+
+    for workspace in get_all_user_workspaces():
+        add_plugins_to_workspace(workspace)
+
+    vs.AlrtDialog(
+        'After you restarted Vectorworks, you just have to add the plugin(s) to your workspace to be able to use them.')
+
+
+# INSTALLATION STEPS ###################################################################################################
+
+set_destination_folder_for('MyPluginName')
+update_to_or_install_dlibrary_version('2015.0.0')
+add_plugins_to_workspaces({'Menus': {'MyMenu': ['MyPluginName']}})
